3432820a78027d6c9fc372407ec5b4f8dbcda2d1
yoshihito5467 <yoshihito5467@outlook.jp>
2017-10-07 12:20:18 +0900

    test

---
+++ PrismTest1/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace PrismTest1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
